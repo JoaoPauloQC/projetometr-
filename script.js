085a53b107b6d9a7d00814a88c45414e680c18da
JoaoPauloQC <joaopauloqueirozcosta@gmail.com>
2024-08-24 18:53:58 -0300

    mudanças e preparação para o javascript

---
+++ script.js
@@ -0,0 +1,19 @@
+const imageb = document.getElementById("#imgfunb")
+const imageg = document.querySelector(".imgf")
+const imager = document.querySelector(".imgf")
+const images = document.querySelector(".imgf")
+const imagey = document.querySelector(".imgf")
+const imagep = document.querySelector(".imgf")
+
+imageb.addEventListener("click", function change () {
+    if (imageb.src === "imgs/pngtree-circle-clipart-dark-green-circle-png-image_2382001-removebg-preview.png"){
+        imageb.src = "imgs/png-transparent-round-red-icon-computer-icons-red-polka-dot-red-button-icon-color-sphere-website-thumbnail-removebg-preview.png";
+    }
+    else  {
+        imageb.src == "imgs/pngtree-circle-clipart-dark-green-circle-png-image_2382001-removebg-preview.png";
+        
+    }
+
+    }
+)
+
